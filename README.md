eaaa072329f4a39cc22ac0b2ebc137665970db28
Stephen Sage <144445980+sage-code82@users.noreply.github.com>
2024-01-16 11:03:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Template for ReactJS TailwindCSS and Vite
+
+A simple template to help get me started on any new React projects, as I am awful at remembering what I have to install!
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-center text-red-700">
+        Hello There! This is a React Template
+      </h1>
+    </>
+  );
+}
+
+export default App;
