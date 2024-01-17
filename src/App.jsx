b5cc74921e3be433c7ecd716e269ec6433a8e72b
Stephen <sagelvl40@gmail.com>
2024-01-17 12:48:55 +0000

    add offers section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Hero from "./components/hero";
 import Navbar from "./components/navbar";
+import Offers from "./components/offers";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Offers />
     </div>
   );
 }
---
+++ src/components/offers.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Offers = () => {
+  return (
+    <div className="max-w-[900px] m-auto px-4 py-12 flex flex-wrap justify-between">
+      <p classNames="text-lg font-bold text-slate-900">Booking Offers</p>
+      <p classNames="text-lg font-bold text-slate-900">Competitive Rates</p>
+      <p classNames="text-lg font-bold text-slate-900">Flexible Booking</p>
+      <p classNames="text-lg font-bold text-slate-900">Trip Insurance</p>
+    </div>
+  );
+};
