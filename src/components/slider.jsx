956d6ca8608ec2e3751240e4aadc135f7e799374
Stephen <sagelvl40@gmail.com>
2024-01-24 16:33:23 +0000

    start work on the new slider

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,57 +1,55 @@
 import React, { useState } from "react";
-import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
-import { RxDotFilled } from "react-icons/rx";
+import img_a from "../assets/endor.jpg";
+import img_b from "../assets/hoth.jpg";
+import img_c from "../assets/tatooine.jpg";
+import img_d from "../assets/kashyyyk.jpg";
+import img_e from "../assets/mustafar.jpg";
+import img_f from "../assets/coruscant.jpg";
+
+// ...
 
 const Slider = () => {
   const slides = [
-    { url: "/endor.jpg", title: "Endor" },
-    { url: "/tatooine.jpg", title: "Tatooine" },
-    { url: "/hoth.jpg", title: "Hoth" },
-    { url: "/kashyyyk.jpg", title: "Kashyyyk" },
-    { url: "/mustafar.jpg", title: "Mustafar" },
-    { url: "/coruscant.jpg", title: "Coruscant" },
+    { img: img_a, title: "Endor" },
+    { img: img_c, title: "Tatooine" },
+    { img: img_b, title: "Hoth" },
+    { img: img_d, title: "Kashyyyk" },
+    { img: img_e, title: "Mustafar" },
+    { img: img_f, title: "Coruscant" },
   ];
 
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const prevSlide = () => {
-    const newIndex = currentIndex === 0 ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
-
-  const nextSlide = () => {
-    const newIndex = currentIndex === slides.length - 1 ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
-
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex);
-  };
   return (
-    <div className="bg-purple-200 max-w-[1400px] h-[580px] w-full m-auto py-16 px-4 relative group">
-      <div
-        className="w-full h-full bg-center bg-cover rounded-2xl duration-500"
-        style={{
-          backgroundImage: `url(${slides[currentIndex].url})`,
-        }}
-      ></div>
-      <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer">
-        <BsChevronCompactLeft onClick={prevSlide} size={35} />
-      </div>
-      <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer">
-        <BsChevronCompactRight onClick={nextSlide} size={35} />
-      </div>
-      <div className="flex top-4 justify-center py-2">
-        {slides.map((slide, slideIndex) => (
-          <div
-            className="text-2xl cursor-pointer"
-            key={slideIndex}
-            onClick={() => goToSlide(slideIndex)}
-          >
-            <RxDotFilled />
-          </div>
+    <div className="container bg-purple-200">
+      <div className="fixed flex flex-col gap-12 items-center justify-center h-screen w-[10%]">
+        {slides.map((bullet, index) => (
+          <img
+            key={index}
+            src={bullet.img}
+            alt={bullet.title}
+            className="w-12 h-12 rounded-full object-cover cursor-pointer"
+          />
         ))}
       </div>
+      {slides.map((slide, index) => (
+        <section key={index} className="h-screen flex snap-start">
+          <div className="w-[35%]">
+            <div className="flex leading-8 flex-col items-center justify-center h-full">
+              <h2 className="text-2xl">{slide.title}</h2>
+            </div>
+          </div>
+          <div className="w-[65%]"></div>
+          <div
+            className="mask"
+            style={{
+              backgroundImage: `url(${slide.img})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              height: "100%",
+              width: "100%",
+            }}
+          ></div>
+        </section>
+      ))}
     </div>
   );
 };
