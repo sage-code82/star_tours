6f18c5e59fee163f0f28727d8f00742ab5c96a3f
Stephen <sagelvl40@gmail.com>
2024-01-24 16:37:28 +0000

    think about redoing from the begininng

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -6,8 +6,6 @@ import img_d from "../assets/kashyyyk.jpg";
 import img_e from "../assets/mustafar.jpg";
 import img_f from "../assets/coruscant.jpg";
 
-// ...
-
 const Slider = () => {
   const slides = [
     { img: img_a, title: "Endor" },
@@ -21,35 +19,39 @@ const Slider = () => {
   return (
     <div className="container bg-purple-200">
       <div className="fixed flex flex-col gap-12 items-center justify-center h-screen w-[10%]">
-        {slides.map((bullet, index) => (
-          <img
-            key={index}
-            src={bullet.img}
-            alt={bullet.title}
-            className="w-12 h-12 rounded-full object-cover cursor-pointer"
-          />
-        ))}
+        {slides.map((bullet, index) => {
+          return (
+            <img
+              src={bullet.img}
+              className={`bullet-${
+                index + 1
+              }w-12 h-12 rounded-full object-cover`}
+            />
+          );
+        })}
       </div>
-      {slides.map((slide, index) => (
-        <section key={index} className="h-screen flex snap-start">
-          <div className="w-[35%]">
-            <div className="flex leading-8 flex-col items-center justify-center h-full">
-              <h2 className="text-2xl">{slide.title}</h2>
+      {slides.map((slide) => {
+        return (
+          <section className="h-screen flex snap-start">
+            <div className="w-[35%]">
+              <div className="flex leading-8 flex-col items-center justify-center h-full">
+                <h2 className="text-2xl">{slide.title}</h2>
+              </div>
             </div>
-          </div>
-          <div className="w-[65%]"></div>
-          <div
-            className="mask"
-            style={{
-              backgroundImage: `url(${slide.img})`,
-              backgroundPosition: "center",
-              backgroundSize: "cover",
-              height: "100%",
-              width: "100%",
-            }}
-          ></div>
-        </section>
-      ))}
+            <div className="w-[65%]"></div>
+            <div
+              className="mask"
+              style={{
+                backgroundImage: `url(${slide.img})`,
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                height: "100%",
+                width: "100%",
+              }}
+            ></div>
+          </section>
+        );
+      })}
     </div>
   );
 };
